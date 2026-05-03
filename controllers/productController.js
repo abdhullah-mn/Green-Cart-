35ad26b027935fdafaf2ee4e012bd069e3f7abde
abdhullah-mn <abdnou076@gmail.com>
2026-05-03 15:42:47 +0530

    successfully created adding product function

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,13 +4,30 @@
 
 // add product : /api/product/add
 export const addProduct = async(req,res)=>{
-
     try{
+        let productData = JSON.parse(req.body.productData);
+        const image = req.file;
+        let imagesUrl = await promise.all(
+            image.map(async(item)=>{
 
-        
+                let result = await cloudinary.uploader.upload(item.path, {resourse_type: "image"});
+                return result.secure_url;
+
+            })
+        )
+    
+        await Product.create({
+            name: productData.name,
+            description: productData.description,
+            images: imagesUrl
+        });
+        res.json({success: true, message: "Product added successfully"});
 
 
     }catch(error){
+
+        console.log(error);
+        res.json({success: false, message: "Failed to add product"});
 
     }
 
